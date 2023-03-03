8c6ff2f4984fb3daf0d1d996a27fa51c42a2e317
Selvam6994 <selvamyoor@gmail.com>
2023-03-03 23:13:44 +0530

    signup and login added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ dotenv.config()
 import express from "express"; // "type": "module"
 import CORS from "cors";
 import { MongoClient } from "mongodb";
+import bcrypt from "bcrypt"
 const app = express();
 
 const PORT = process.env.PORT;
@@ -37,4 +38,58 @@ app.post(
     response.send(result);
   }
 );
+
+async function generateHashedPassword(password){
+  const NO_OF_ROUNDS = 10;
+  const salt =await bcrypt.genSalt(NO_OF_ROUNDS);
+  const hashedPassword = await bcrypt.hash(password,salt)
+  // console.log(salt);
+  // console.log(hashedPassword);
+  return hashedPassword
+}
+
+
+app.post("/mobileData/signup",express.json(),async function(request,response){
+  const {username,email,password} = await request.body;
+
+  const userFromDB = await client.db("MobilePhones").collection("signUpData").findOne({username:username})
+  console.log(userFromDB);
+  if(userFromDB){
+    response.status(401).send({message:"Already exists"})
+  }else{
+    const hashedPassword = await generateHashedPassword(password)
+    const result = await client.db("MobilePhones").collection("signUpData").insertOne({
+      username:username,
+      email:email,
+      password:hashedPassword
+    });
+    console.log(hashedPassword);
+    response.status(200).send(result)
+  }
+ 
+})
+
+app.post("/mobileData/login",express.json(),async function(request,response){
+  const {username,email,password} = await request.body;
+  const userFromDB = await client.db("MobilePhones").collection("signUpData").findOne({email:email})
+  if(!userFromDB){
+    response.status(401).send({message:"Invalid credentials"})
+  }else{
+    const result  = await client.db("MobilePhones").collection("signUpData").findOne({
+      username:username,
+      email:email,
+      password:password
+    })
+    response.status(200).send({
+      message:"Logged in successfully"
+    })
+  }
+  
+})
+app.get("/mobileData/signup",async function (request,response){
+  const data = await client.db("MobilePhones").collection("signUpData").find({}).toArray();
+})
+
+
+
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
